715dffc9242694c763cb8ed1cb659aad5f7fe51e
Silvia-Wachira <slywachira96@gmail.com>
2023-10-20 22:37:19 +0300

    check email

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,7 @@
-const validator = require('./')
+const validator = require('./node_modules/validator')
 const getNotes = require('./notes')
 const printNotes = getNotes()
 
 console.log(printNotes)
 
-console.log(validator.isEmail('example.com'))
+console.log(validator.('example.com'))
