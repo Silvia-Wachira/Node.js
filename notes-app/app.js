8b577952936cfa5ea09ef7a3b85004cf62bb2427
Silvia-Wachira <slywachira96@gmail.com>
2023-10-20 23:10:16 +0300

    istall chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,5 @@
-const validator = require('./node_modules/validator')
 const getNotes = require('./notes')
 const printNotes = getNotes()
 
 console.log(printNotes)
 
-console.log(validator.('example.com'))
