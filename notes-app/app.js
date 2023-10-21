f5db5c07b5dae6438e4d2a293ab52e33f1f4f6a7
Silvia-Wachira <slywachira96@gmail.com>
2023-10-21 23:30:39 +0300

    customize yargs version

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,14 +1,8 @@
-const { require } = require('yargs')
 const chalk = require('./node_modules/chalk')
 const yargs = require('yargs')
 const getNotes = require('./notes')
 
-const command = process.argv[2]
+//Customize yargs version
+yargs.version('1.1.0')
 
-console.log(process.argv)
-
-if (command === 'add') {
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    console.log('Remove note!')
-}
+console.log(yargs.argv)
