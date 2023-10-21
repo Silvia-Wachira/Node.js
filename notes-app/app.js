87a8ee752b0bb1014e88ebe80a2ab78b56563577
Silvia-Wachira <slywachira96@gmail.com>
2023-10-21 23:20:47 +0300

    load yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,11 @@
+const { require } = require('yargs')
 const chalk = require('./node_modules/chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes')
 
 const command = process.argv[2]
+
+console.log(process.argv)
 
 if (command === 'add') {
     console.log('Adding note!')
