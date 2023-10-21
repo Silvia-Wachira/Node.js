886f0d6b497ac010eed46af50ed4bd59ffc262ba
Silvia-Wachira <slywachira96@gmail.com>
2023-10-21 23:42:40 +0300

    remove the note

--- notes-app/app.js
+++ notes-app/app.js
@@ -15,4 +15,12 @@ yargs.command({
 })
 //add , remove , read, list
 
+//Create remove command
+yargs.command({
+    command:'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
 console.log(yargs.argv)
