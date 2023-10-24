5ab92821c9642f7b9fc13339fef5b0585aed00d9
Silvia-Wachira <slywachira96@gmail.com>
2023-10-25 00:40:45 +0300

    store data with json

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,17 +1,6 @@
 const fs = require('fs')
 const { join } = require('path')
-// const book = {
-//     title: 'Ego is the Enemy',
-//     author: 'Ryan Holiday'
-// }
 
-// const bookJSON = JSON.stringify(book)
-// fs.writeFileSync('1-json.json',bookJSON)
-
-// const dataBuffer = fs.readFileSync('1-json.json') 
-// const dataJSON = dataBuffer.toString()
-// const data = JSON.parse(dataJSON)
-// console.log(data.title) 
 
 
 const dataBuffer = fs.readFileSync('1-json.json')
