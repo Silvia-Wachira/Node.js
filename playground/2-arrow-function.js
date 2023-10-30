c4753c2c1d80c8db2bbb4d777e0661168ae52ece
Silvia-Wachira <slywachira96@gmail.com>
2023-10-30 22:32:26 +0300

    create a new file

--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -2,7 +2,23 @@
 //     return x * x 
 // }
 
-const square = (x) => {
-    return x * x
+// const square = (x) => {
+//     return x * x
+// }
+
+// const square = (x) => x * x
+
+// console.log(square(2))
+
+const event = {
+    name: 'Birthday Party',
+    guestList: ['Andrew', 'Jen', 'Mike'],
+    printGuestList() {
+       console.log('Guest list for '  + this.name) 
+       this.guestList.forEach( (guest) => {
+            console.log(guest + ' is attending ' + this.name)
+       }) 
+    }
 }
-console.log(square(3))
+
+event.printGuestList()
