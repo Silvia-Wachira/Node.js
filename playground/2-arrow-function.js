1cfa17b6c5d1b0a83e6d513e8fbfa317c98f13ae
Silvia-Wachira <slywachira96@gmail.com>
2023-10-30 14:02:43 +0300

    create an arrow fn

--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -1,4 +1,8 @@
-const square = function (x) {
-    return x * x 
+// const square = function (x) {
+//     return x * x 
+// }
+
+const square = (x) => {
+    return x * x
 }
 console.log(square(3))
