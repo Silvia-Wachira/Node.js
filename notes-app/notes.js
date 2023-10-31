87ec4ca3fe318010e7ec73a6a34c3306fb0edc46
Silvia-Wachira <slywachira96@gmail.com>
2023-10-31 14:50:58 +0300

    read command

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -65,9 +65,21 @@ const listNotes = () => {
     });
 }
 
+const readNotes = (title) => {
+    const notes = loadNotes()
+        const note = notes.find((note) => note.title === title)
+        if (note) {
+            console.log(chalk.inverse(note.title))
+            console.log(note.body)
+        } else {
+            console.log(chalk.red.inverse('Note not found!'))
+        }
+}
+
 module.exports = {
     getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
-    listNotes: listNotes
+    listNotes: listNotes,
+    readNotes: readNotes
 }
